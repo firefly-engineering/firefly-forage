0e65f4b7fc95dd4eccfbd93cea60bc4ec78473c2
Yann Hodique <yann@firefly.engineering>
2026-02-20 16:27:59 +0100

    fix: update vendorHash after removing BurntSushi/toml dependency

--- packages/forage-ctl/default.nix
+++ packages/forage-ctl/default.nix
@@ -11,7 +11,7 @@ buildGoModule {
 
   src = ./.;
 
-  vendorHash = "sha256-GexDCgbFueOXsiSBBmJb14b7gC5g9P27BE72U/vV2+A=";
+  vendorHash = "sha256-QKnRD3WkrrfrWvRZM6P2E3tzbvqI25c5RG9IEd/U7Kg=";
 
   # Disable CGO for static build
   env.CGO_ENABLED = "0";
